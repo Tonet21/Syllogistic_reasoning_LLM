fead51bd9cb0f8f3c542d7464d362ab10ad471fe
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2024-07-09 11:37:21 +0200

    Update syllos.py

--- syllos.py
+++ syllos.py
@@ -1,18 +1,27 @@
-import random
-from collections import OrderedDict
-
-
 ## This code is originally from the blog post
 ##http://prolatio.blogspot.com/2016/08/all-256-syllogism-forms-with-examples.html
-## I just modiefied it a little to get the syllogisms how I wanted the and then
+## I modified it a little to get the syllogisms how I wanted them, and then
 ## I added code to make and append the possible conclusions (be aware that the
-## code still needs cleaning and it is not as efficient as it should)
+## code still needs cleaning, and it is not as efficient as it should)
 
-sylloExamples = [('actuaries', 'sculptors', 'writers')]
+sylloExamples = [('actuaries', 'sculptors', 'writers'), ('assistants', 'poets', 'scientists'),
+                 ('athletes', 'assistants', 'chefs'), ('chemists', 'drivers', 'dancers'), 
+                 ('chemists', 'workers', 'painters'), ('clerks', 'butchers', 'athletes'), 
+                 ('dancers', 'bankers', 'riders'), ('doctors', 'riders', 'investors'), 
+                 ('drivers', 'porters', 'chemists'), ('farmers', 'surfers', 'writers'),
+                 ('gamblers', 'cleaners', 'models'), ('golfers', 'cyclists', 'assistants'),
+                 ('hunters', 'analysts', 'swimmers'), ('joggers', 'actors', 'carpenters'),
+                 ('linguists', 'cooks', 'models'), ('linguists', 'skaters', 'singers'),
+                 ('managers', 'clerks', 'butchers'), ('miners', 'tellers', 'poets'), 
+                 ('models', 'tailors', 'florists'), ('nurses', 'scholars', 'buyers'),
+                 ('planners', 'sailors', 'engineers'), ('riders', 'agents', 'waiters'),
+                 ('riders', 'novelists', 'linguists'), ('runners', 'opticians', 'clerks'),
+                 ('scientists', 'novelists', 'florists'), ('skaters', 'barbers', 'cooks'),
+                 ('students', 'cashiers', 'doctors'), ('students', 'hikers', 'designers'),
+                 ('surfers', 'painters', 'porters'), ('therapists', 'hikers', 'opticians')]
 
 ## sylloExamples contains the triplets of terms; these terms could be changed
 ## depending on if we want them to have semantic relation or not
-
 
 wordType = {"S": "subject",
             "M": "middle",
@@ -40,15 +49,15 @@ class Figure:
 
 
 class Mood:
-    linkMapping = {"a": "all ",
-                   "e": "no ",
-                   "i": "some ",
-                   "o": "some ",
+    linkMapping = {"A": "all ",
+                   "E": "no ",
+                   "I": "some ",
+                   "O": "some ",
     }
-    postVerbLink = {"a": "",
-                    "e": "",
-                    "i": "",
-                    "o": "not "}
+    postVerbLink = {"A": "",
+                    "E": "",
+                    "I": "",
+                    "O": "not "}
 
     def __init__(self, links):
         self.links = links
@@ -56,11 +65,9 @@ class Mood:
         self.minorLink = links[1]
 
 class Syllogism:
-    def __init__(self, figure, mood, syllowords=None):
+    def __init__(self, figure, mood, syllowords):
         self.figure = Figure(figure)
         self.mood = Mood(mood)
-        if syllowords is None:
-            syllowords = random.choice(sylloExamples)
         self.subject = syllowords[0]
         self.middle = syllowords[1]
         self.predicate = syllowords[2]
@@ -86,108 +93,23 @@ class Syllogism:
 
 def generateAll():
     all_generated_syllogisms = []
-    Syllogisms = []
-    for i in range(1, 5):
-        for a in "aeio":
-            for b in "aeio":
-                for c in "aeio":
-                    syllogisms = []
-                    s = Syllogism(i, a + b + c)
-                    syllogism = s.generate()
-                    Syllogisms.append(syllogism)
-                    syllogisms.append([syllogism[0]])
-                    syllogisms.append([syllogism[1]])
-                    all_generated_syllogisms.append(syllogisms)
+    possible_conclusions = []
+    for terms in sylloExamples:
+        for i in range(1, 5):
+            for a in "AEIO":
+                for b in "AEIO":         
+                         s = Syllogism(i, a + b, terms)
+                         syllogism = s.generate() + [a + b]
+                         all_generated_syllogisms.append(syllogism)
+                         choices = [f"""All {terms[0]} are {terms[2]}""",
+                          f"""No {terms[0]} are {terms[2]}""",
+                          f"""Some {terms[0]} are {terms[2]}""",
+                          f"""Some {terms[0]} are not {terms[2]}""" ]
+                         possible_conclusions.append(choices)
 
 
 
-    return all_generated_syllogisms , Syllogisms
+    return all_generated_syllogisms, possible_conclusions
 
-
-generated_syllogisms = generateAll()[0]
-premises = generateAll()[1]  ##change the name to premises but look  exactly what it does. the code itself works, just clean the names
-
-## generated_syllogisms is a list that contains 256 list, each of them contains
-## two lists (first ans second premise)
-
-
-def CountFrequency(my_list):
-
-
-    freq = {}
-    for item in my_list:
-        if item in freq:
-            freq[item] += 1
-        else:
-            freq[item] = 1
-    return freq
-
-prem_terms = []
-for prem in premises:
-  Arg = ", ".join(prem)
-  Arg = Arg.replace(",", " ")
-  Arg = Arg.split()
-  prems = []
-  for wrd in Arg:
-    if wrd in sylloExamples[0]:
-      prems.append(wrd)
-  prem_terms.append(prems)
-
-
-freq = []
-for i in prem_terms:
-  dic= CountFrequency(i)
-  freq.append(dic)
-
-
-term_count = []
-for dt in freq:
-  list = [(t, s) for t, s in dt.items()]
-  term_count.append(list)
-
-
-Terms = []
-for term in term_count:
-  lst = []
-  for tp in term:
-    if tp[1] == 1:
-      lst.append(tp[0])
-
-
-  Terms.append(lst)
-
-
-
-Possible_conclusions = []
-for term in Terms:
-  S = term[0]
-  P = term[1]
-  conclusion_choices = [
-    "All {} are {}".format(S, P),
-    "Some {} are {}".format(S, P),
-    "No {} are {}".format(S, P),
-    "Some {} are not {}".format(S, P),
-    "All {} are {}".format(P, S),
-    "Some {} are {}".format(P, S),
-    "No {} are {}".format(P, S),
-    "Some {} are not {}".format(P, S),
-    "Nothing follows"
-]
-  Possible_conclusions.append(conclusion_choices)
-
-
-x = 0
-Prompts = []
-for arguments in generated_syllogisms:
-  complete_syllogisms = arguments + [Possible_conclusions[x]]
-  Prompts.append(complete_syllogisms)
-  x += 1
-Complete_prompts = []
-for prompt in Prompts:
-   cprompt = """Choose the conclusion that logically follows the premises, 
-if none of the conclusions logically follows choose 'Nothing follows'.
-
-The premises are: {}{}.
-The possible conclusions are {}.""".format(prompt[0], prompt[1], prompt[2])
-
-   Complete_prompts.append(cprompt)
+syllogisms = generateAll()[0]
+possible_conclusions = generateAll()[1]
