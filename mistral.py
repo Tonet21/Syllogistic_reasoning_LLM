6de6093a088ac1274ca2a2a050d088289cb84d70
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2024-09-08 17:30:36 +0200

    Update and rename Mistral.py to mistral.py

--- mistral.py
+++ mistral.py
@@ -9,11 +9,11 @@ from long_prompt import prompts,system_message
 from mistral_common.protocol.instruct.messages import SystemMessage
 
 
-# Set your Hugging Face access token here
-access_token = "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"
 
-# Define the directory to save the model
-local_model_dir = "/mnt/cimec-storage6/users/antoni.llinasbarbara/Cat_Eng_Syllogistic_reasoning_LLM/models"
+access_token = "hf..."
+
+
+local_model_dir = ""
 mistral_models_path = Path(local_model_dir)
 mistral_models_path.mkdir(parents=True, exist_ok=True)
 
@@ -36,10 +36,10 @@ import re
 import re
 
 def extract_conclusion(text):
-    # Define the pattern to match the specified phrases
+
     pattern = r"(the correct answer is|the valid conclusion is|therefore)\s*(.*)"
 
-    # Search for the pattern in the provided text
+
     match = re.search(pattern, text, re.IGNORECASE)
 
     if match:
@@ -49,14 +49,13 @@ def extract_conclusion(text):
         return text
 
 def remove_quotes(text):
-    # Replace single and double quotes with nothing
+
     return text.replace('"', '').replace("'", "")
 
 def fix_comma_and_capitalization(text):
-    # Remove leading comma and any extra spaces
+
     text = text.lstrip(',').strip()
 
-    # Capitalize the first letter of the text
     if text:
         text = text[0].upper() + text[1:]
 
@@ -67,17 +66,15 @@ model_conclusions = []
 system_message = SystemMessage(content=system_message)
 
 for prompt in prompts:
-# Create the chat completion request
+
     completion_request = ChatCompletionRequest(messages=[system_message, UserMessage(content=prompt)])
 
     tokens = tokenizer.encode_chat_completion(completion_request).tokens
 
-# Generate the response
+
     out_tokens, _ = generate([tokens], model, max_tokens=256, temperature=0.1, eos_id=tokenizer.instruct_tokenizer.tokenizer.eos_id)
     result = tokenizer.instruct_tokenizer.tokenizer.decode(out_tokens[0])
-    #result = extract_conclusion(result)
-    print("-----------------------------")
-    print(result)
-    #result = remove_quotes(result)
-    #result = fix_comma_and_capitalization(result)
+    result = extract_conclusion(result)
+    result = remove_quotes(result)
+    result = fix_comma_and_capitalization(result)
     model_conclusions.append(result)
