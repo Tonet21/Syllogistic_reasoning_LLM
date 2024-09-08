2183b05cbea9afc95533443f8b5597ca7c106f97
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2024-09-08 17:21:24 +0200

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,28 +1,28 @@
 from syllo import syllogisms, possible_conclusions
 
+system_message = f"""You are an expert in syllogistic reasoning.
+                     Your role is to evaluate a syllogism and select the valid conclusion from a list of four options.
+                     You will be provided with two premises and four possible conclusions in the following format:
 
+                     Premise 1: <premise 1>
+                     Premise 2: <premise 2>
+                     Choices: <list of choices separated by commas>
+
+                     Your response should be one of the given choices, or "NVC" if none of the options are valid.
+
+                     Please take your time to analyze the syllogism carefully and avoid rushing to a conclusion.
+                     """
 prompts = []
 i = 0
-for premise1, premise2, mood in syllogisms:
-        prompt = f""" 
-    Your task is to choose the valid conclusion for a syllogism. You will be presented 
-    with a list of four choices for the conclusion and the  premises of the syllogism 
-    between three backticks. Your answer should only be one of the choices or <NVC> if 
-    you don't find a valid conclusion. The syllogism will be presented
-    in the following format:
-    Premise 1: <premise 1>
-    Premise 2: <premise 2>
-    Choices: <list of possible conclusions separated by commas>
+for premise1, premise2, mood, type_ in syllogisms:
+        prompt = f"""
 
-    Take your time to answer, do not rush and take it step by step.
-
-    ``` 
-    Premise 1: {premise1}
-    Premise 2: {premise2}
-    Choices: {possible_conclusions[i][0]}, {possible_conclusions[i][1]}, {possible_conclusions[i][2]}, {possible_conclusions[i][3]}
-    ```
+                    Premise 1: {premise1}
+                    Premise 2: {premise2}
+                    Choices: {possible_conclusions[i][0]}, {possible_conclusions[i][1]}, {possible_conclusions[i][2]}, {possible_conclusions[i][3]}
 
     """
         prompts.append(prompt)
         i += 1
+
 
