ef4060f9fd634561f83f2df07b4a1d4df40b2b07
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2024-09-19 11:01:55 +0200

    Update llama3.py

--- llama3.py
+++ llama3.py
@@ -15,13 +15,11 @@ model = AutoModelForCausalLM.from_pretrained(
     token=access_token
 )
 
-import re
-
 def extract_conclusion(text):
-    # Regex pattern to match the conclusion
+
     pattern = r"Conclusion:\s*(All.*?|Some.*?|No.*?|NVC.*?)\s*\n"
 
-    # Search for the pattern in the provided text
+
     match = re.search(pattern, text, re.DOTALL)
 
     if match:
@@ -30,10 +28,9 @@ def extract_conclusion(text):
         return None
 
 
-# Initialize an empty list to store the model's responses
 model_conclusions = []
 
-# Process each prompt
+
 for prompt in prompts:
     # Combine the system message and user prompt
     messages = [
@@ -41,7 +38,6 @@ for prompt in prompts:
         {"role": "user", "content": prompt},
     ]
 
-    # Apply chat template to format the conversation
     input_ids = tokenizer.apply_chat_template(
         messages,
         add_generation_prompt=True,
@@ -49,13 +45,11 @@ for prompt in prompts:
     ).to(model.device)
 
 
-    # Define the terminators
     terminators = [
         tokenizer.eos_token_id,
         tokenizer.convert_tokens_to_ids("")
     ]
 
-    # Generate a response using the model with the specified temperature and top_p
     outputs = model.generate(
     input_ids,
     max_new_tokens=256,
@@ -65,7 +59,6 @@ for prompt in prompts:
     top_p=0.9,
         )
 
-    # Decode the generated tokens to text
     response = outputs[0][input_ids.shape[-1]:]
     decoded_response = tokenizer.decode(response, skip_special_tokens=True)
     #decoded_response = extract_conclusion(decoded_response)
